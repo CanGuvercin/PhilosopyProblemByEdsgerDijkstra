77ba6bc23d02c8bb7d05f6096d21aaeec74b865d
Can Guvercin <mustafacanguvercin@gmail.com>
2025-05-27 01:57:51 +0300

    Main app established

--- src/main/java/eu/guvercin/DiningPhilosophersProblem.java
+++ src/main/java/eu/guvercin/DiningPhilosophersProblem.java
@@ -1,4 +1,21 @@
 package eu.guvercin;
 
 public class DiningPhilosophersProblem {
+    public static void main(String[] args){
+        int numberOfPhilosophers = 5;
+        Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
+        Fork[] forks = new Fork[numberOfPhilosophers];
+
+        for (int i=0; i < numberOfPhilosophers; i++){
+            forks[i] = new Fork(i);
+        }
+        for (int i=0; i< numberOfPhilosophers; i++){
+            Fork leftFork = forks[i];
+            Fork rightFork = forks[(i + 1) % numberOfPhilosophers];
+
+            philosophers[i] = new Philosopher(i, leftFork, rightFork);
+            Thread thread = new Thread(philosophers[i], "Philosopher " +i);
+            thread.start();
+        }
+    }
 }
